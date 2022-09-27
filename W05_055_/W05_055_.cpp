702b8ec467ae1aeb266fc80d73b3ce3fd9b46995
sso9s <114461598+sso9s@users.noreply.github.com>
2022-09-27 13:44:19 +0900

    Add files via upload

---
+++ W05_055_/W05_055_.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+int main()
+{
+	int n;
+
+	// *
+	// **
+	// ***
+	// ****
+	// *****
+	printf("n을 입력 : ");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+
+
+	int m;
+
+	// *
+	// ***
+	// *****
+	// *******
+	// *********
+	// i: 1 ~ 5
+	// j: 1 ~ 2*i-1
+	printf("m을 입력 : ");
+	scanf_s("%d", &m);
+
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+}
---
+++ W05_056_gugudan/W05_056_gugudan.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+// 구구단(5단)
+/*
+	5x1=5
+	5x2=10
+	5x3=15
+	5x4=20
+	..
+	5x9=45
+	두번째 숫자가 1~9까지 변화(반복문 1개 필요)
+	*/
+
+int main()
+{
+	int n;
+
+	printf("n을 입력: ");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= 9; i++)
+		printf("%d x %d = %d\n", n, i, n*i);
+
+	// 
+
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 2; j <= 9; j++)
+			printf("%d x %d = %d\t", j, i, j*i);
+		printf("\n");
+	}
+
+	int m;
+
+	printf("m을 입력: ");
+	scanf_s("%d", &m);
+
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= 2 * i - 1; j++)
+			printf("%d", j);
+	
+}
